bd4015696663e0d939bd7011bc17bf58af0e836a
psicho <9400878@gmail.com>
2017-09-06 13:30:31 +0400

    Задача «a+b» решена

---
+++ 001.py
@@ -0,0 +1,11 @@
+# Задача «a+b»
+
+with open("input.txt") as file:
+
+    for line in file.readlines():
+        line = line.split(' ')
+        line = int(line[0]) + int(line[1])
+
+    with open('output.txt', 'w') as file:
+        file.write(str(line))
+
