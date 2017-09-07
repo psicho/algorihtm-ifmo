eb262b8b8a316aa27ee5a7980a3d09a1eb5bddc1
psicho <9400878@gmail.com>
2017-09-07 13:58:41 +0400

    Задача решена, но решение не проходит проверку. Пишет: "В выходном файле содержится лишняя информация (например, дополнительные числа)."

--- 003.py
+++ 003.py
@@ -7,40 +7,35 @@ with open("input.txt") as file:
     line = file.readlines(2)[0].replace(' ','')
     sort = list(line)
     sdvig = dict()
-    # print(sort)
+    stru = ''
     for i in range(1,k):
         j = i - 1
         n = i
         s = 0
         dig = sort[i]
-        pos = i
-        sdvig[dig] = pos + 1
+        sdvig[dig] = i + 1
         while j >= 0:
             if int(sort[n]) < int(sort[j]):
                 temp = sort[j]
                 sort[j] = sort[n]
                 sort[n] = temp
-                # print(sort)
-                sdvig[dig] = j
-                sdvig[sort[n]] = n
-                # sdvig[dig] = j
                 j = j - 1
                 n = n - 1
                 s = s + 1
                 continue
-            else: break
-            # print(sort)
+            else:
+                break
+        if i == 1:
+            stru = stru + str(n) + ' '
+            sdvig[line[i-1]] = 1
+            stru = stru + str(n+1) + ' '
         if s > 0:
-            sdvig[dig] = j + 2
-    print(sdvig)
-    print(sort)
-            # sort = sort + line[i] + ' '
-
-
-    # for line in file.readlines():
-    #     line = line.split(' ')
-    #     line = int(line[0]) + int(line[1])*int(line[1])
-
+            stru = stru + str(n+1) + ' '
+            sdvig[dig] = n + 1
+        if i > 1 and s == 0:
+            stru = stru + str(n+1) + ' '
+    sort = str(sort).replace('[','').replace(']','').replace("'",'').replace(',','').strip()
+    stru = stru.strip()
 
 with open('output.txt', 'w') as file:
-    file.write(str(sort))
+    file.write(stru + '\n' + sort)
