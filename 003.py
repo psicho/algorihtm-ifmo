57a89729c558e71e9108d5d7b170c1965ae07f5b
psicho <9400878@gmail.com>
2017-09-08 13:42:58 +0400

    Удалены лишние строки и действия в коде.

--- 003.py
+++ 003.py
@@ -10,15 +10,12 @@ with open("input.txt") as file:
     sdvig = dict()
     stru = ''
 
-
     stru = stru + '1' + ' '
 
     for i in range(1,k):
         j = i - 1
         n = i
         s = 0
-        dig = sort[i]
-        sdvig[dig] = i + 1
         while j >= 0:
             if int(sort[n]) < int(sort[j]):
                 temp = sort[j]
@@ -30,12 +27,10 @@ with open("input.txt") as file:
                 continue
             else:
                 break
-        if i == 1 and s > 0:
-            sdvig[line[i-1]] = 1
+        if i == 1:
             stru = stru + str(n+1) + ' '
         if s > 0 and i > 1:
             stru = stru + str(n+1) + ' '
-            sdvig[dig] = n + 1
         if i > 1 and s == 0:
             stru = stru + str(n+1) + ' '
     sort = str(sort).replace('[','').replace(']','').replace("'",'').replace(',','').strip()
